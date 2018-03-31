c01d2726fa0fe86335b42cf8e03f1909e84cedf7
Pedro R <rpedro13@hotmail.com>
2018-03-31 16:15:11 -0400

    Se arreglo el menu user en toolbar
    Se coloco mas info en el menu emergente y se utilizo un objeto
    como data binding

--- src/app/home/toolbar/toolbar.component.ts
+++ src/app/home/toolbar/toolbar.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+interface user{
+  fullName: string;
+  email: string;
+  imageUrl: string;
+  id: string;
+}
+
+
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent implements OnInit {
 
+export class ToolbarComponent implements OnInit {
+  userInfo: user = {
+    fullName: "Maynard J. Keenan",
+    email: "maynard@tool.com",
+    imageUrl: "/assets/img/perfil.jpg",
+    id: "1",
+  }
   constructor() { }
 
   ngOnInit() {
