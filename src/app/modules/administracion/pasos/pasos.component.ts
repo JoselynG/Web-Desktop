f6888878f2933b0aeef263af568b99f9c67be1d3
Jose <jose120293@gmail.com>
2018-04-27 01:52:19 -0400

    cambios

--- src/app/modules/administracion/pasos/pasos.component.ts
+++ src/app/modules/administracion/pasos/pasos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { variable } from '@angular/compiler/src/output/output_ast';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material';
 
 @Component({
   selector: 'app-pasos',
@@ -6,15 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pasos.component.scss']
 })
 export class PasosComponent implements OnInit {
-  listaParametro: any[];
-  listaValor: any[];
+  [x: string]: any;
+  consejo: Array<{}> = [];
+  datoBasico: Array<{}> = [];
+  listaParametro: Array<{}> = [];
+  listaValor: Array<{}> = [];
   tipoparametro: any;
     listaTipoParametro: Array<{id_dato: string, nombre: string, status: string, tipo_dato: string }>;
 
   listaParametros: Array<{id_dato: string, id_parametro: string, nombre: string, tipo: string}>;
 
   listavalorparametro: Array<{id_valor: string, id_parametro: string, nombre: string, descripcion: string}>;
-  constructor() {
+  constructor(public dialog: MatDialog) {
     this.listaTipoParametro = [
       {id_dato: '1', nombre: 'Sexo', status: 'a', tipo_dato: 'basico'},
       {id_dato: '2', nombre: 'Rango de edad', status: 'a', tipo_dato: 'basico'},
@@ -35,10 +40,10 @@ export class PasosComponent implements OnInit {
     ];
 
     this.listavalorparametro = [
-      {id_valor: '1', id_parametro: '4', nombre: 'Largo', descripcion: 'El rostro es estirado'},
-      {id_valor: '1', id_parametro: '4', nombre: 'Redondo', descripcion: 'El redondeado'},
-      {id_valor: '1', id_parametro: '5', nombre: 'Ondulado', descripcion: 'su cabello presenta ondas'},
-      {id_valor: '1', id_parametro: '5', nombre: 'Crespo', descripcion: 'es un cabello dificil de manejar'},
+      {id_valor: '1', id_parametro: '5', nombre: 'Largo', descripcion: 'El rostro es estirado'},
+      {id_valor: '2', id_parametro: '5', nombre: 'Redondo', descripcion: 'El redondeado'},
+      {id_valor: '3', id_parametro: '6', nombre: 'Ondulado', descripcion: 'su cabello presenta ondas'},
+      {id_valor: '4', id_parametro: '6', nombre: 'Crespo', descripcion: 'es un cabello dificil de manejar'},
     ];
   }
 
@@ -46,17 +51,35 @@ export class PasosComponent implements OnInit {
 
       }
   cargarParametro(id) {
+    let j = 0;
+    this.listaParametro = [];
     for (let i = 0; i < this.listaParametros.length; i++) {
       if (this.listaParametros[i].id_dato === id) {
-        this.listaParametro.push(this.listaParametros[i]);
+        this.listaParametro[j] = this.listaParametros[i];
+        console.log(id);
+        j++;
+        console.log(this.listaParametro);
       }
     }
   }
   cargarValorParametro(id) {
-    for (let i = 0; i < this.listaParametro.length; i++) {
+    let j = 0;
+    this.listaValor = [];
+    for (let i = 0; i < this.listavalorparametro.length; i++) {
         if (this.listavalorparametro[i].id_parametro === id) {
-          this.listaValor.push(this.listavalorparametro);
+          this.listaValor[j] = this.listavalorparametro[i];
+          console.log(id);
+          j++;
         }
     }
+  }
+  guardarAtributo(data) {
+    this.consejo.push(data);
+    console.log(this.consejo);
+  }
+  guardarDatoBasico(data) {
+    this.datoBasico.push(data);
+  }
+  Guardar() {
   }
 }
