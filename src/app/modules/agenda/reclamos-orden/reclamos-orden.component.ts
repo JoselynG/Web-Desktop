a666d4e3f71ad9d61cecdfe09afa952ec76e773e
JoselynG <joselyngab_93@hotmail.com>
2018-04-30 15:25:48 -0400

    cambios

--- src/app/modules/agenda/reclamos-orden/reclamos-orden.component.ts
+++ src/app/modules/agenda/reclamos-orden/reclamos-orden.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
+
+
 interface Datos_reclamo{
 	nombre: string;
 	orden: string;
-	fecha: string;
+	fecha: Date;
 	tipoR: string;
 	descripcion: string;
 	
-	fechaV:string;
+	fechaV:Date;
 
 }
 
@@ -19,26 +21,26 @@ interface Datos_reclamo{
   styleUrls: ['./reclamos-orden.component.scss']
 })
 export class ReclamosOrdenComponent implements OnInit {
-
+ hoy= new Date();
   datos: Datos_reclamo[]= [
     {
       nombre: 'Maria Anzola',
       orden: '001',
-      fecha: '10 abril 2018',
+      fecha: new Date('2018/04/10'),
       tipoR: 'producto',
       descripcion:'El producto que se utilizo cuando me aplicaron tratamiento de keratina, me causó alergia, pido pronta atencion y me regresen mi dinero.',
     
-    fechaV: '20 abril 2018'
+    fechaV: new Date('2018/04/20')
     },
 
     {
         nombre: 'Yanior Zambrano',
         orden: '002',
-        fecha: '11 abril 2018',
+        fecha: new Date('2018/04/11'),
         tipoR: 'servicio',
         descripcion:' Me hicieron mal el corte, me di cuenta al irme de la peluqueria',
         
-        fechaV: '20 abril 2018'
+        fechaV: new Date('2018/05/20')
     }
 
 
@@ -71,8 +73,8 @@ export class DarRepuestaComponent {
 //selec pregunta
 filtroSelec = '';
 filtro = [
-  {value: 'positiva', viewValue: 'positiva'},
-  {value: 'negativa', viewValue: 'negativa'},
+  {value: 'positiva', viewValue: 'Procede'},
+  {value: 'negativa', viewValue: 'No procede'},
   
 ];
 
--- src/app/modules/portal-web/galeria/galeria.component.ts
+++ src/app/modules/portal-web/galeria/galeria.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GaleriaComponent implements OnInit {
 
+  titulo_seccion:ITitulo_Seccion={titulo:"Galeria de imagenes",descripcion:"Descripcion de galeria de imagenes"};
+
+  imagenes:IImagen[]=[
+    {
+      titulo:"titulo 1", imagen:"/assets/img/perfil.jpg", descripcion:"descripcion 1"
+    },
+    {
+      titulo:"titulo 2", imagen:"/assets/img/perfil.jpg", descripcion:"descripcion 2"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  agregarImagen(imag,titu,descr) { // se llama cada vez que ocurre el evento change del file input
+    if (imag && imag[0]) {
+      let reader = new FileReader();
+      reader.readAsDataURL(imag[0]); //lee el archivo como datos del url
+      reader.onload = (event:any) => { //se llama una vez que se lea del url
+        this.imagenes.push({titulo:titu,imagen:event.target.result,descripcion:descr});
+      }
+    }
+  }
+
+}
+
+interface IImagen{
+  id?:number;
+  id_sistema?:number;
+  titulo:string;
+  imagen:string;
+  descripcion:string;
+  estatus?:string;
+}
+interface ITitulo_Seccion{
+  id?:number;
+  id_sistema?:number;
+  titulo:string;
+  descripcion:string;
+  boton?:string;
+  estatus?:string;
 }
